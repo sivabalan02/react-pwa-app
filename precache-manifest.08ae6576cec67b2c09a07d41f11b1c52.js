32daa2e5aaa4d7da16d47437216e40d9c08fe2c2
sivabalan <sivabalan@kuyatechnologies.com>
2019-12-02 16:57:40 +0530

    Updates

---
+++ precache-manifest.08ae6576cec67b2c09a07d41f11b1c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833143d91b5c1a1e830aa0d45bdef069",
+    "url": "/react-pwa-app/index.html"
+  },
+  {
+    "revision": "23767a134ec58f4b5308",
+    "url": "/react-pwa-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f68a0b4e5e6ce9511c0c",
+    "url": "/react-pwa-app/static/js/2.6a98df9b.chunk.js"
+  },
+  {
+    "revision": "23767a134ec58f4b5308",
+    "url": "/react-pwa-app/static/js/main.9772ebc9.chunk.js"
+  },
+  {
+    "revision": "e35e4efb1f9c4ed436e1",
+    "url": "/react-pwa-app/static/js/runtime-main.8e289e6d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-pwa-app/static/media/logo.25bf045c.svg"
+  }
+]);
